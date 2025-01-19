2fa518082ef87d93a777c8fd29b179cddf725dbc
Joshua Vine <joshuavine54@gmail.com>
2025-01-19 13:45:03 +0400

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Nav from "./components/Nav"
+import Hero from "./sections/Hero"
 export default function App(){
   return(
-    <>
+    <main className="relative">
       <Nav />
-    </>
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      
+    </main>
     // <main className="relative">
       
     //   <Nav />
