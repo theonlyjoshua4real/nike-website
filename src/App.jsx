571f05c927f8773bdf66040261c0696ca85bee4a
Joshua Vine <joshuavine54@gmail.com>
2025-01-20 06:02:38 +0400

    Popular Products Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Nav from "./components/Nav"
 import Hero from "./sections/Hero"
+import PopularProducts from "./sections/PopularProducts"
 export default function App(){
   return(
     <main className="relative">
@@ -7,6 +8,12 @@ export default function App(){
       <section className="xl:padding-l wide:padding-r padding-b">
         <Hero />
       </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      
+
+
       
     </main>
     // <main className="relative">
